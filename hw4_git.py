05e2c169c25ffc25140a12c7322cfe8feaca4008
Sarah Zhao <sarahzhao@Sarahs-MacBook-Pro.local>
2020-02-11 11:30:20 -0500

    change hw4_git

--- hw4_git.py
+++ hw4_git.py
@@ -1,1 +1,14 @@
 print("Hello World")
+res = input("Any number between 1 to 3")
+while True:
+    if int(res) == 1:
+        print("Good luck!")
+        break
+    elif int(res) == 2:
+        print("Happy New Year!")
+        break
+    elif int(res) == 3:
+        print("Go Ahead!")
+        break
+    else:
+        break
